c562d07c89de879e9b5d2943aff94004ab17903b
C-F-K <a.fell.ninny.mocker@gmail.com>
2014-03-04 16:25:53 +0100

    grab firebase json

--- week4/week4.js
+++ week4/week4.js
@@ -1,1 +1,10 @@
-;
+var data = new Object();
+
+d3.json("http://ast.firebaseio.com/.json", function(err, parsedJSON) {
+  if (err) {
+    console.log(err);
+    return err;
+  } else {
+    data = parsedJSON;
+  }
+});
